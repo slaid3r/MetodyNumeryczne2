56eb3ddfced98471ab822320b1913438be652919
slaid3r <slaid3r@gmail.com>
2015-03-29 10:43:44 +0200

    Main class added.

---
+++ src/main/java/MetodyNumeryczne2.java
@@ -0,0 +1,14 @@
+/**
+ * Application main class.
+ */
+public class MetodyNumeryczne2 {
+
+    /**
+     * Application entry point.
+     *
+     * @param args  the array of arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
